08f538cf4d67e8b10a1aa8ce7991409bfdb55183
Diego Aquino <diegocruzdeaquino@gmail.com>
2023-04-07 20:33:52 -0300

    Adicionar testes de email de funcionário

--- src/test/java/com/project/salarycalculator/EmployeeTest.java
+++ src/test/java/com/project/salarycalculator/EmployeeTest.java
@@ -7,8 +7,9 @@ import org.junit.Test;
 public class EmployeeTest {
 
   @Test
-  public void testEmployeeShouldHaveAName() {
-    Employee employee = new Employee("Name");
+  public void testEmployeeShouldInitializeCorrectly() {
+    Employee employee = new Employee("Name", "email@email.com");
     assertEquals("Name", employee.getName());
+    assertEquals("email@email.com", employee.getEmail());
   }
 }
