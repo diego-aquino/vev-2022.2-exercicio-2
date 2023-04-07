e44d90002d1361e16542107308008b747977dac5
Diego Aquino <diegocruzdeaquino@gmail.com>
2023-04-07 20:36:00 -0300

    Adicionar email de funcionário

--- src/main/java/com/project/salarycalculator/Employee.java
+++ src/main/java/com/project/salarycalculator/Employee.java
@@ -3,12 +3,18 @@ package com.project.salarycalculator;
 public class Employee {
 
   private String name;
+  private String email;
 
-  public Employee(String name) {
+  public Employee(String name, String email) {
     this.name = name;
+    this.email = email;
   }
 
   public String getName() {
     return name;
+  }
+
+  public String getEmail() {
+    return email;
   }
 }
