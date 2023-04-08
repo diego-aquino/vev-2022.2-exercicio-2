d80850a27ee787aa0b48c4981021d145ea29426b
Diego Aquino <diegocruzdeaquino@gmail.com>
2023-04-07 21:20:17 -0300

    Adicionar cálculo de salário desenvolvedor igual ou acima do limite

--- src/main/java/com/project/salarycalculator/DeveloperEmployeeSalaryCalculator.java
+++ src/main/java/com/project/salarycalculator/DeveloperEmployeeSalaryCalculator.java
@@ -2,10 +2,17 @@ package com.project.salarycalculator;
 
 public class DeveloperEmployeeSalaryCalculator {
 
-  private static final double DISCOUNT = 0.1;
+  private static final int SALARY_THRESHOLD_IN_CENTS = 300000;
+  private static final double SALARY_DISCOUNT_BELOW_THRESHOLD = 0.1;
+  private static final double SALARY_DISCOUNT_AT_OR_ABOVE_THRESHOLD = 0.2;
 
   public int calculateSalaryInCents(Employee employee) {
     int baseSalaryInCents = employee.getBaseSalaryInCents();
-    return (int) Math.floor(baseSalaryInCents * (1 - DISCOUNT));
+
+    if (baseSalaryInCents < SALARY_THRESHOLD_IN_CENTS) {
+      return (int) Math.floor(baseSalaryInCents * (1 - SALARY_DISCOUNT_BELOW_THRESHOLD));
+    } else {
+      return (int) Math.floor(baseSalaryInCents * (1 - SALARY_DISCOUNT_AT_OR_ABOVE_THRESHOLD));
+    }
   }
 }
