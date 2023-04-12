9c1ccb6684b82059f2eca3d3e4cd79a454389a79
Rob <bruno.robsiq@gmail.com>
2023-04-12 09:30:00 -0300

    Adicionar teste de construtor de Pagamento

---
+++ src/main/java/com/project/billprocessor/Payment.java
@@ -0,0 +1,6 @@
+package com.project.billprocessor;
+
+public class Payment {
+    public Payment() {
+    }
+}
---
+++ src/test/java/com/project/billprocessor/PaymentTest.java
@@ -0,0 +1,22 @@
+package com.project.billprocessor;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class PaymentTest {
+
+  @Test
+  public void newPaymentHasPaidAmountDatePaymentKind() {
+    long paidAmount = 1500 * 100;
+    Date date = new Date();
+    String paymentKind = "BOLETO";
+    Payment payment = new Payment(paidAmount, date, paymentKind);
+
+    assertEquals(paidAmount, payment.getPaidAmount());
+    assertEquals(date, payment.getDate());
+    assertEquals(paymentKind, payment.getPaymentKind());
+  }
+}
