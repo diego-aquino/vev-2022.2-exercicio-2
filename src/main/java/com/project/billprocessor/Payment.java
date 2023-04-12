849c50be7b2bb79780918408ae6b69fee8631405
Rob <bruno.robsiq@gmail.com>
2023-04-12 09:35:37 -0300

    Adicionar construtor de Pagamento

--- src/main/java/com/project/billprocessor/Payment.java
+++ src/main/java/com/project/billprocessor/Payment.java
@@ -1,6 +1,27 @@
 package com.project.billprocessor;
 
+import java.util.Date;
+
 public class Payment {
-    public Payment() {
-    }
+  private long paidAmount;
+  private Date date;
+  private String paymentKind;
+
+  public Payment(long paidAmount, Date date, String paymentKind) {
+    this.paidAmount = paidAmount;
+    this.date = date;
+    this.paymentKind = paymentKind;
+  }
+
+  public long getPaidAmount() {
+    return paidAmount;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public String getPaymentKind() {
+    return paymentKind;
+  }
 }
