ac5d7e418886b5ff517ee84ac4e1276d814bca3a
Diego Aquino <diegocruzdeaquino@gmail.com>
2023-04-12 13:52:55 -0300

    Simplificar nomes de variáveis nos testes de funcionário

--- src/test/java/com/project/employee/EmployeeTest.java
+++ src/test/java/com/project/employee/EmployeeTest.java
@@ -6,46 +6,41 @@ import org.junit.Test;
 
 public class EmployeeTest {
 
-  private String employeeName = "Name";
-  private String employeeEmail = "email@email.com";
-  private int employeeBaseSalaryInCents = 300000;
+  private String name = "Name";
+  private String email = "email@email.com";
+  private int baseSalaryInCents = 300000;
 
   @Test
   public void testEmployeeShouldInitializeCorrectly() {
-    Employee employee = new Employee(
-      employeeName,
-      employeeEmail,
-      employeeBaseSalaryInCents,
-      EmployeePosition.DEVELOPER
-    );
+    Employee employee = new Employee(name, email, baseSalaryInCents, EmployeePosition.DEVELOPER);
 
-    assertEquals(employeeName, employee.getName());
-    assertEquals(employeeEmail, employee.getEmail());
-    assertEquals(employeeBaseSalaryInCents, employee.getBaseSalaryInCents());
+    assertEquals(name, employee.getName());
+    assertEquals(email, employee.getEmail());
+    assertEquals(baseSalaryInCents, employee.getBaseSalaryInCents());
     assertEquals(EmployeePosition.DEVELOPER, employee.getPosition());
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testEmployeeShouldThrowAnErrorIfNameIsNull() {
-    String nullEmployeeName = null;
-    new Employee(nullEmployeeName, employeeEmail, employeeBaseSalaryInCents, EmployeePosition.DEVELOPER);
+    String nullName = null;
+    new Employee(nullName, email, baseSalaryInCents, EmployeePosition.DEVELOPER);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testEmployeeShouldThrowAnErrorIfEmailIsNull() {
-    String nullEmployeeEmail = null;
-    new Employee(employeeName, nullEmployeeEmail, employeeBaseSalaryInCents, EmployeePosition.DEVELOPER);
+    String nullEmail = null;
+    new Employee(name, nullEmail, baseSalaryInCents, EmployeePosition.DEVELOPER);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testEmployeeShouldThrowAnErrorIfBaseSalaryIsNegative() {
-    int negativeEmployeeBaseSalaryInCents = -1;
-    new Employee(employeeName, employeeEmail, negativeEmployeeBaseSalaryInCents, EmployeePosition.DEVELOPER);
+    int negativeBaseSalaryInCents = -1;
+    new Employee(name, email, negativeBaseSalaryInCents, EmployeePosition.DEVELOPER);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testEmployeeShouldThrowAnErrorIfPositionIsNull() {
-    EmployeePosition nullEmployeePosition = null;
-    new Employee(employeeName, employeeEmail, employeeBaseSalaryInCents, nullEmployeePosition);
+    EmployeePosition nullPosition = null;
+    new Employee(name, email, baseSalaryInCents, nullPosition);
   }
 }
