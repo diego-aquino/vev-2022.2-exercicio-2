00bbbe2314577963b15f233a35d0c20f23485d40
Diego Aquino <diegocruzdeaquino@gmail.com>
2023-04-12 13:41:34 -0300

    Adicionar testes de funcionário com valores null e negativos

--- src/test/java/com/project/employee/EmployeeTest.java
+++ src/test/java/com/project/employee/EmployeeTest.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 
 public class EmployeeTest {
 
+  private String employeeName = "Name";
+  private String employeeEmail = "email@email.com";
+  private int employeeBaseSalaryInCents = 300000;
+
   @Test
   public void testEmployeeShouldInitializeCorrectly() {
-    String employeeName = "Name";
-    String employeeEmail = "email@email.com";
-    int employeeBaseSalaryInCents = 300000;
-
     Employee employee = new Employee(
       employeeName,
       employeeEmail,
@@ -23,5 +23,29 @@ public class EmployeeTest {
     assertEquals(employeeEmail, employee.getEmail());
     assertEquals(employeeBaseSalaryInCents, employee.getBaseSalaryInCents());
     assertEquals(EmployeePosition.DEVELOPER, employee.getPosition());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testEmployeeShouldThrowAnErrorIfNameIsNull() {
+    String nullEmployeeName = null;
+    new Employee(nullEmployeeName, employeeEmail, employeeBaseSalaryInCents, EmployeePosition.DEVELOPER);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testEmployeeShouldThrowAnErrorIfEmailIsNull() {
+    String nullEmployeeEmail = null;
+    new Employee(employeeName, nullEmployeeEmail, employeeBaseSalaryInCents, EmployeePosition.DEVELOPER);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testEmployeeShouldThrowAnErrorIfBaseSalaryIsNegative() {
+    int negativeEmployeeBaseSalaryInCents = -1;
+    new Employee(employeeName, employeeEmail, negativeEmployeeBaseSalaryInCents, EmployeePosition.DEVELOPER);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testEmployeeShouldThrowAnErrorIfPositionIsNull() {
+    EmployeePosition nullEmployeePosition = null;
+    new Employee(employeeName, employeeEmail, employeeBaseSalaryInCents, nullEmployeePosition);
   }
 }
