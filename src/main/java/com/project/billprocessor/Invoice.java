3683db1a051abc38619c250f6392c1cf502b7928
Rob <bruno.robsiq@gmail.com>
2023-04-12 10:00:15 -0300

    Adicionar lista de boletos em Fatura

--- src/main/java/com/project/billprocessor/Invoice.java
+++ src/main/java/com/project/billprocessor/Invoice.java
@@ -1,16 +1,24 @@
 package com.project.billprocessor;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Invoice {
   private Date date;
   private long totalAmount;
   private String clientName;
+  private List<Bill> billList;
 
   public Invoice(Date date, long totalAmount, String clientName) {
     this.date = date;
     this.totalAmount = totalAmount;
     this.clientName = clientName;
+    this.billList = new ArrayList<Bill>();
+  }
+
+  public List<Bill> getBillList() {
+    return billList;
   }
 
   public Date getDate() {
