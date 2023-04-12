a6ddf82bc6d0a0a4c7a258a73973b9792d42f248
Rob <bruno.robsiq@gmail.com>
2023-04-12 09:15:21 -0300

    Adicionar construtor de Boleto

--- src/main/java/com/project/billprocessor/Bill.java
+++ src/main/java/com/project/billprocessor/Bill.java
@@ -1,6 +1,28 @@
 package com.project.billprocessor;
 
+import java.util.Date;
+
 public class Bill {
-  public Bill() {
+  private String code;
+  private Date date;
+
+  private long paidAmount;
+
+  public Bill(String code, Date date, long paidAmount) {
+    this.code = code;
+    this.date = date;
+    this.paidAmount = paidAmount;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public long getPaidAmount() {
+    return paidAmount;
   }
 }
