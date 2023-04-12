e405e1d8c1a96db0e4cde0a2fc0162572637c043
Rob <bruno.robsiq@gmail.com>
2023-04-12 20:21:04 -0300

    Adicionar class de destino de Pagamento

--- src/main/java/com/project/billprocessor/PaymentDest.java
+++ src/main/java/com/project/billprocessor/PaymentDest.java
@@ -1,4 +1,16 @@
 package com.project.billprocessor;
 
 public enum PaymentDest {
+  BILL("BOLETO");
+
+  // enum with title
+  private String title;
+
+  private PaymentDest(String title) {
+    this.title = title;
+  }
+
+  public String getTitle() {
+    return title;
+  }
 }
